50c6325408ccd244997a07d9730450e8731200fa
Jacob Olshanskiy <101607869+JacobProgrammer@users.noreply.github.com>
2022-10-13 23:36:31 -0700

    Update HomePersonal.java

--- HomePersonal.java
+++ HomePersonal.java
@@ -1,12 +1,11 @@
-
 /**
  * Calculates Home Prices and Preferences
- *
+ * All values for budget before 1,000,000 are without the zeros
+ * e.g. 600 = 600,000
  * @author (Jacob Olshanskiy)
  * @version (10-6-22)
  */
 import java.util.Scanner;
-
 public class HomePersonal
 {
    public static int price()
@@ -17,14 +16,6 @@ public class HomePersonal
         price.nextLine();
         return readPrice;
     }
-    
-   public static String greeting()
-   {
-       Scanner greeting = new Scanner(System.in);
-       System.out.println("Great let me do some research");
-       String readGreeting = greeting.nextLine();
-       return readGreeting;
-   }
    
    public static void main(String[] args)
     {
@@ -32,22 +23,38 @@ public class HomePersonal
         Scanner homestate = new Scanner(System.in);
         System.out.println("What state are you shopping in?");
         String readHomestate = homestate.nextLine();
-        if (readHomestate == "Alabama")
-        {
-            int userPrice = price();
-            greeting();
-            if(userPrice > 1000000)
+        int userPrice = price();
+        if (homestate.equals("Alabama") || homestate.equals("alabama"))
+            if (userPrice > 1000000)
             {
                 System.out.println("The optimal city for you to shop in is Orange Beach");
             }
-            else if(999 > userPrice && userPrice > 500)
-            {
-                System.out.println("The optimal city for you to shop in is Vesatvia Hills");             
+        
+            else if (userPrice < 999 && userPrice > 500)
+            {   
+                System.out.println("The optimal city for you to shop in is Vesatvia Hills");
             }
-            else
+            else 
             {
-                System.out.println("The optimal city for you to shop in is Hoover"); 
+                System.out.println("The optimal city for you to shop in is Hoover");
             }
+        
+        int userPrice2 = price();
+      
+        if (homestate.equals("Alaska") || homestate.equals("alaska"))
+        if (userPrice2 > 1000000)
+        {
+            System.out.println("The optimal city for you to shop in is The optimal city for you to shop is Juneau");
+        }
+        
+        else if (userPrice2 < 999 && userPrice2 > 500)
+        {
+            System.out.println("The optimal city for you to shop in is Anchorage");
+        }
+        
+        else
+        {
+            System.out.println("The optimal city for you to shop in is Fairbanks");
         }
     }
 }
