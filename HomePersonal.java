ac2596b57826512cfc79a3c1f8549a4896faf61f
Jacob Olshanskiy <101607869+JacobProgrammer@users.noreply.github.com>
2022-10-20 23:36:41 -0700

    Update HomePersonal.java

--- HomePersonal.java
+++ HomePersonal.java
@@ -17,7 +17,6 @@ public class HomePersonal
     price.nextLine();
     return readPrice; 
   }
-  
   public static void main(String[] args)
   {
     System.out.println("Hello! I will help you find the home of your dreams");
@@ -25,7 +24,6 @@ public class HomePersonal
             System.out.println("What state are you shopping in?");
             String readHomestate = homestate.nextLine();
             int userPrice = price();
-
 
             if (readHomestate.equals(("Alabama")) || readHomestate .equals(("alabama")) || (readHomestate.equals("AL")))
                 if (userPrice > 1000000)
@@ -226,11 +224,146 @@ public class HomePersonal
                 {
                     System.out.println("The optimal city for you to shop in is Star");
                 }
-        
+
+            if (readHomestate.equals(("Illinois")) || readHomestate.equals(("illinois")) || readHomestate.equals("IL"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Hinsdale");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Burr Ridge");
+                }
+     
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Barrington");
+                }
+            
+            if (readHomestate.equals(("Indiana")) || readHomestate.equals(("indiana")) || readHomestate.equals("IN"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Zionsville");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Carmel");
+                }
+     
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Westfield");
+                }   
+
+            if (readHomestate.equals(("Iowa")) || readHomestate.equals(("iowa")) || readHomestate.equals("IA"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Bettendorf");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Johnston");
+                }
+     
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Coralville");
+                }
+
+            if (readHomestate.equals(("Kansas")) || readHomestate.equals(("kansas")) || readHomestate.equals("KS"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Leawood");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Overland Park");
+                }
+     
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Lenexa");
+                }
+
+            if (readHomestate.equals(("Kentucky")) || readHomestate.equals(("kentucky")) || readHomestate.equals("KY"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Nicholasville");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Lexington-Fayette");
+                }
+     
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Union");
+                }
+
+            if (readHomestate.equals(("Louisiana")) || readHomestate.equals(("louisiana")) || readHomestate.equals("LA"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is New Orleans");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Natchitoches");
+                }
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Hammond");
+                }
+
+            if (readHomestate.equals(("Maine")) || readHomestate.equals(("maine")) || readHomestate.equals("ME"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Portland");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Biddeford");
+                }
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Saco");
+                }
+
+            if (readHomestate.equals(("Maryland")) || readHomestate.equals(("maryland")) || readHomestate.equals("MD"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Mount Rainer");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is College Park");
+                }
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Bladensburg");
+                }
+
+            if (readHomestate.equals(("Massachusetts")) || readHomestate.equals(("massachusetts")) || readHomestate.equals("MA"))
+                if (userPrice > 1000000)
+                { 
+                    System.out.println("The optimal city for you to shop is Cambridge");
+                }
+     
+                else if (userPrice < 999 && userPrice > 500)
+                {
+                    System.out.println("The optimal city for you to shop in is Boston");
+                }
+    
+                else
+                {
+                    System.out.println("The optimal city for you to shop in is Somerville");
+                }
     }
 }
-                  
-        
-          
-        
-    
